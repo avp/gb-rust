778126732454b3c8634f2334290913d3066b6a59
Aakash Patel <aakpat6@gmail.com>
2018-06-30 09:34:51 -0700

    [APU] Add stepping framework.

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,3 +1,5 @@
+use std::mem;
+
 struct Square {
   sweep_time: u8, // 3 bit value: sweep_time/128 Hz.
   sweep_dir: i8,  // 1 if increase, -1 if decrease.
@@ -87,6 +89,12 @@ impl Square {
     }
   }
 
+  pub fn step(t: u32) {}
+
+  pub fn next() -> i8 {
+    0
+  }
+
   /// Get the actual 8-bit duty given the duty number.
   fn duty(&self) -> u8 {
     match self.duty_number & 0b11 {
@@ -102,6 +110,11 @@ impl Square {
 pub struct APU {
   channel1: Square,
   channel2: Square,
+
+  sound_select: u8,
+
+  counter: u32,
+  buf: Vec<(i8, i8)>,
 }
 
 impl APU {
@@ -109,6 +122,11 @@ impl APU {
     APU {
       channel1: Square::new(),
       channel2: Square::new(),
+
+      sound_select: 0,
+
+      counter: 0,
+      buf: vec![],
     }
   }
 
@@ -116,6 +134,7 @@ impl APU {
     match addr {
       0xff10...0xff14 => self.channel1.rb(addr - 0xff10),
       0xff20...0xff24 => self.channel2.rb(addr - 0xff20),
+      0xff25 => self.sound_select,
       _ => 0,
     }
   }
@@ -124,7 +143,39 @@ impl APU {
     match addr {
       0xff10...0xff14 => self.channel1.wb(addr - 0xff10, val),
       0xff20...0xff24 => self.channel2.wb(addr - 0xff20, val),
+      0xff25 => self.sound_select = val,
       _ => (),
     }
+  }
+
+  pub fn step(&mut self, t: u32) {
+    if t > 4 {
+      self.step(4);
+      self.step(t - 4);
+      return;
+    }
+
+    if self.counter & ((1 << 13) & 1) == 0 {
+      let idx = (self.counter >> 13) & 0x7;
+      self.channel1.step(idx);
+      self.channel2.step(idx);
+    }
+
+    self.counter += t;
+
+    let (s1, s2) = (self.channel1.next(), self.channel2.next());
+    let ss = self.sound_select;
+
+    let c1 =
+      if ss & 0x01 != 0 { s1 } else { 0 } + if ss & 0x02 != 0 { s2 } else { 0 };
+    let c2 =
+      if ss & 0x10 != 0 { s1 } else { 0 } + if ss & 0x20 != 0 { s2 } else { 0 };
+
+    self.buf.push((c1, c2));
+  }
+
+  /// Dump the APU buffer to the caller. Clears the buffer.
+  pub fn dump(&mut self) -> Vec<(i8, i8)> {
+    mem::replace(&mut self.buf, vec![])
   }
 }
