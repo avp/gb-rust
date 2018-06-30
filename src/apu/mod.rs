20ed76587cc016c91d5492093d361ec1e77fe6b6
Aakash Patel <aakpat6@gmail.com>
2018-06-29 20:07:47 -0700

    Implement apu wb.

--- src/apu/mod.rs
+++ src/apu/mod.rs
@@ -1,17 +1,18 @@
 struct Square {
   sweep_time: u8, // 3 bit value: sweep_time/128 Hz.
-  sweep_dir: u8,  // 1 if increase, -1 if decrease.
+  sweep_dir: i8,  // 1 if increase, -1 if decrease.
   sweep_shift: u8,
 
   duty_number: u8, // 2-bit index into duty table.
-  length: u8,      // 5-bit length => actual length is (64 - length) / 256.
+  length: u8,      // 5-bit length => actual length is length / 256.
 
   env_volume: u8, // Initial volume, between 0 and 0xf.
-  env_dir: u8,    // 1 for increase, -1 for decrease.
+  env_dir: i8,    // 1 for increase, -1 for decrease.
   env_sweep: u8,  // Length of a step is env_sweep / 64.
 
   frequency: u16,       // Actual frequency: 131072/(2048-x) Hz.
   length_enabled: bool, // If true, stop output when length expires.
+  triggered: bool,
 }
 
 impl Square {
@@ -30,6 +31,7 @@ impl Square {
 
       frequency: 0,
       length_enabled: false,
+      triggered: false,
     }
   }
 
@@ -53,6 +55,33 @@ impl Square {
         let mid = if self.length_enabled { 1 } else { 0 };
         // Only bit 6 is readable.
         0xbf | (mid << 6)
+      }
+      _ => unreachable!(),
+    }
+  }
+
+  pub fn wb(&mut self, addr: u16, val: u8) {
+    match addr {
+      0 => {
+        self.sweep_time = (val >> 4) & 0x7;
+        self.sweep_dir = if val & 0x08 == 0 { 1 } else { -1 };
+        self.sweep_shift = val & 0x7;
+      }
+      1 => {
+        self.length = 64 - (val & 0x3f);
+      }
+      2 => {
+        self.env_volume = (val >> 4) & 0x7;
+        self.env_dir = if val & 0x08 == 0 { -1 } else { 1 };
+        self.env_sweep = val & 0x7;
+      }
+      3 => {
+        self.frequency = (self.frequency & !0xff) | (val as u16);
+      }
+      4 => {
+        self.frequency = (self.frequency & 0xff) | ((val as u16 & 0x7) << 8);
+        self.length_enabled = val & 0x40 != 0;
+        self.triggered = val & 0x80 != 0;
       }
       _ => unreachable!(),
     }
